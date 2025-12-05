d65bb67355c03a08fa26872f21783ab2462fd43c
Omar <omar.j.black@gmail.com>
2025-12-05 09:28:32 -0500

    added functional button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,22 @@
-import React, { useState} from "react";
-import { StyleSheet, TextInput } from 'react-native';
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, Button, StyleSheet, TextInput} from 'react-native';
 
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError ] = useState(null);
+  const [userInput, setUserInput ] = useState("");
+  const [currencyIn, setCurrencyIn] = useState("CAD");
+  const [currencyOut, setCurrencyOut] = useState("USD");
+  const [conversion, setConversion ] = useState("");
+  const [message, setMessage ] = useState("")
+
+  const handleButtonPress = () => {
+    setMessage(`${userInput} ${currencyOut}`);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Currency Converter</Text>
@@ -16,6 +29,8 @@ export default function TabOneScreen() {
             style={styles.currency_input}
             placeholder="CAD"
             placeholderTextColor="white"
+            onChangeText={setCurrencyIn}
+            value={currencyIn}
         />
 
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
@@ -24,24 +39,25 @@ export default function TabOneScreen() {
             style={styles.currency_input}
             placeholder="USD"
             placeholderTextColor="white"
+            onChangeText={setCurrencyOut}
+            value={currencyOut}
         />
 
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         <Text>Amount:</Text>
         <TextInput
             style={styles.currency_input}
-            placeholder="100"
-            placeholderTextColor="white"
+            onChangeText={setUserInput}
+            value={userInput}
+
+        />
+        <Button
+            title="Convert"
+            onPress={handleButtonPress}
         />
 
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        <Text>Conversion:</Text>
-        <TextInput
-            style={styles.currency_input}
-            placeholder="100"
-            placeholderTextColor="white"
-        />
-
+        <Text>Conversion: {message}</Text>
       </View>
     </View>
   );
@@ -68,9 +84,14 @@ const styles = StyleSheet.create({
   },
   currency_input: {
     height: 50,
-    // width: '100%',
-    borderColor: 'white',
+    borderColor: 'black',
     borderWidth: 1,
+    color: 'white',
+  },
+  currency_output: {
+    height: 70,
+    borderColor: 'black',
+    borderWidth: 2,
     color: 'white',
   },
 });
